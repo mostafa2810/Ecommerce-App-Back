a9d404eed3aff757668e46f7f2ea0df0447defa9
mostafa2810 <mostafa.helaly2810@gmail.com>
2024-03-19 22:01:34 +0200

    first commit

---
+++ OrderManagement/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using ProductCatalog.Entities;
+
+namespace OrderManagement.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public Customer Customer { get; set; }
+        public Product Product { get; set; }
+    }
+}
